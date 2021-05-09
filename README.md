e1a90000807116dc14c8c6998402bea5751e8cac
João Rodrigues <joao.devmail@gmail.com>
2021-05-09 10:38:41 -0300

    Seeds adicionadas

--- README.md
+++ README.md
@@ -12,6 +12,6 @@ Aplicativo Todo com Rails
 
 1. Entre na pasta do projeto e instale as gems: `bundle install`
 2. Instale as dependências yarn com o comando: `yarn`
-3. Faça as migrações do banco de dados embarcado: `rake db:migrate`
+3. Faça as migrações do banco de dados e carregue as seeds: `rails db:setup`
 4. Execute o servidor: `rails server`
 5. Entre com o seu navegador na URL: `http://localhost:3000`
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+Todo.create(
+    [
+        {titulo: 'Assistir Filme', descricao: "Assistir %s" % [Faker::Movie.title], concluido: Faker::Boolean.boolean(true_ratio: 0.4)},
+        {titulo: 'Escutar Música', descricao: "Escutar %s" % [Faker::Music.band], concluido: Faker::Boolean.boolean(true_ratio: 0.4)},
+        {titulo: 'Escutar Música', descricao: "Escutar %s" % [Faker::Music.band], concluido: Faker::Boolean.boolean(true_ratio: 0.4)},
+        {titulo: 'Assistir Filme', descricao: "Assistir %s" % [Faker::Movie.title], concluido: Faker::Boolean.boolean(true_ratio: 0.4)},
+        {titulo: 'Assistir Filme', descricao: "Assistir %s" % [Faker::Movie.title], concluido: Faker::Boolean.boolean(true_ratio: 0.4)},
+        {titulo: 'Jogar Video Game', descricao: "Jogar %s" % [Faker::Game.title], concluido: Faker::Boolean.boolean(true_ratio: 0.4)},
+        {titulo: 'Jogar Video Game', descricao: "Jogar %s" % [Faker::Game.title], concluido: Faker::Boolean.boolean(true_ratio: 0.4)},
+        {titulo: 'Jogar Video Game', descricao: "Jogar %s" % [Faker::Game.title], concluido: Faker::Boolean.boolean(true_ratio: 0.4)},
+        {titulo: 'Assistir Filme', descricao: "Assistir %s" % [Faker::Movie.title], concluido: Faker::Boolean.boolean(true_ratio: 0.4)},
+    ]
+)
